8b41d9be63d9067115cccdb894ae3c41917e2436
Vittorio <mauriello.v@gmail.com>
2017-11-03 19:45:49 +0100

    x

---
+++ _eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "google",
+    "parserOptions": { 
+        "ecmaVersion": 6 
+    },
+    "rules": {
+        "comma-dangle": 0,
+        "spaced-comment": 0,
+        "max-len": 0,
+        "require-jsdoc": 0,
+    }
+};
--- js/app.js
+++ js/app.js
@@ -272,7 +272,7 @@ const todoApp = (function() {
         toggleAllInput: document.getElementById('toggleAll'),
         todoCountSpan: document.getElementById('todoCountSpan'),
         clearCompletedBtn: document.getElementById('clearCompletedBtn'),
-        todoTemplate: function() {
+        TodoTemplate: function() {
           let elements = {
             //todosDiv: document.createElement('div'),
             todosLi: document.createElement('li'),
@@ -297,7 +297,7 @@ const todoApp = (function() {
       return todosUI;
     };
     const renderTodo = (todo, ui) => {
-      let template = new ui.todoTemplate();
+      let template = new ui.TodoTemplate();
       let todoTextWithCompletion = '';
       if (todo.completed === true) {
         template.todosCheckbox.checked = true;
@@ -417,8 +417,15 @@ const switcher = (function() {
     xhr.setRequestHeader('Cache-Control', 'no-cache');
     //
     xhr.onreadystatechange = function() {
+<<<<<<< HEAD
       if (this.readyState !== 4) { return; } //eslint-disable-line
       if (this.status !== 200) {return;}// or whatever error handling you want
+=======
+      if (this.readyState !== 4) { return; } // eslint-disable-line  
+      if (this.status !== 200) {
+        return; // or whatever error handling you want // eslint-disable-line
+      }
+>>>>>>> 2189666a55c61df3abf6aad70ef721bcf3b1e7e6
       playGround.innerHTML = this.responseText;
     };
     return xhr;
@@ -530,7 +537,7 @@ const noteApp = (function() {
         section.appendChild(fileref);
         return fileref;
       } else if (filetype === 'css') { //if filename is an external CSS file
-        var fileref = document.createElement('link');
+        let fileref = document.createElement('link');
         fileref.setAttribute('rel', 'stylesheet');
         fileref.setAttribute('type', 'text/css');
         fileref.setAttribute('href', filename);
@@ -744,6 +751,7 @@ const noteApp = (function() {
       let mediaBtns = mediaDiv.querySelectorAll('buttons');
       let mediaSrcDiv = mediaDiv.querySelector('#mediaSrc');
       let mediaSrcInput = mediaSrcDiv.querySelector('input');
+<<<<<<< HEAD
       let leftMostBound = quill.getBounds(quill.getSelection(quill.setSelection(0))).left;
 
       const mediaBarRule = () => {
@@ -786,40 +794,86 @@ const noteApp = (function() {
         };
         quill.on('text-change', backtrace);
         // quill.off('text-change', backtrace);
+=======
+      // let leftMostBound = quill.getBounds(quill.getSelection(quill.setSelection(0))).left;
+      let Block = Quill.import('blots/block'); //TEMP
+
+      const mediaBarRule = () => {
+        //ok it works need to hide it on selection change with an emitter
+        quill.on('editor-change', function(type, range) {
+          if (type !== 'selection-change') {
+            return;
+          }
+          if (range === null) {
+            return;
+          }
+          let [block, offset] = quill.scroll.descendant(Block, range.index);
+          if (block !== null && block.domNode.firstChild instanceof HTMLBRElement) {
+            mediaDiv.className = 'active';
+          } else {
+            mediaDiv.className = 'hidden';
+          }
+        });
+>>>>>>> 2189666a55c61df3abf6aad70ef721bcf3b1e7e6
       };
 
-      const setUpMediaBtns = () => {
+      const setUpMediaEls = () => {
         mediaBtns.forEach(function(btn, i) {
           btn.addEventListener('click', function(e) {
-            let mediaType = this.getAttribute('media');
-            applyMedia(this, mediaType);
+            let mediaType = btn.getAttribute('media');
+            // getMedia(btn, mediaType);
+            listenforSrc(mediaType);
           });
         });
       };
-
-      //I can setup formatting bar functions as to used them here
-      const applyMedia = (btn, mediaType) => {
-        if (btn.selected) {
-          getMediaSrc(mediaType);
+      const listenforSrc = (mediaType) => {
+        if (mediaType === 'divider') {
+          applyMedia(mediaType);
+          resetMediaBar();
+          return;
         }
+        displayMediaSrcBar();
+        mediaSrcInput.addEventListener('keyup', function(e) {
+          if (e.keyCode === ENTER_KEY) {
+            let src = this.value;
+            applyMedia(mediaType, src);
+            resetMediaBar();
+          }
+        });
+      };
+      const resetMediaBar = () => {
+        //remove listener from mediaSrcInput
+        let clone = mediaSrcInput.cloneNode(true);
+        mediaSrcDiv.replaceChild(clone, mediaSrcInput);
+        mediaSrcInput = clone;
+        commons.changeVisibility([mediaDiv, mediaSrcDiv]);
+      };
+
+      const displayMediaSrcBar = () => {
         commons.changeVisibility([mediaDiv, mediaSrcDiv]);
         mediaSrcInput.focus();
-        mediaSrcTooltipFix();
+        // mediaSrcTooltipFix();
       };
-      const getMediaSrc = (mediaType, url) => {
-        if (url) {
-          quill.format(mediaType, url);
+
+      const applyMedia = (mediaType, src) => {
+        let range = quill.getSelection(true);
+        if (!src && mediaType === 'divider') {
+          quill.insertEmbed(range.index, 'divider', true, Quill.sources.USER);
+          quill.setSelection(range.index + 1, Quill.sources.SILENT);
         } else {
-          quill.format(mediaType, false);
-          //TODO:net to select bt based on attr mediaType
-          // linkbtn.selected = true;
+          quill.insertEmbed(range.index, mediaType, src, true, Quill.sources.USER);
+          quill.setSelection(range.index + 1, Quill.sources.SILENT);
         }
-        commons.changeVisibility([formats, linkBar]);
-        linkInput.value = '';
+        // commons.changeVisibility([formats, linkBar]);
+        mediaSrcInput.value = '';
         resetToolbarView();
       };
 
       mediaBarRule();
+<<<<<<< HEAD
+=======
+      setUpMediaEls();
+>>>>>>> 2189666a55c61df3abf6aad70ef721bcf3b1e7e6
     };
 
     const addKeyBindings = (fmtBar) => {
@@ -892,15 +946,13 @@ const noteApp = (function() {
         static create(value) {
           let node = super.create();
           // node.setAttribute('alt', value.alt);
-          node.setAttribute('src', value.url);
+          node.setAttribute('src', src);
           return node;
         }
 
         static value(node) {
-          return {
-            // alt: node.getAttribute('alt'),
-            url: node.getAttribute('src')
-          };
+          let src = node.getAttribute('src');
+          return src;
         }
       }
       ImageBlot.blotName = 'image';
@@ -917,7 +969,7 @@ const noteApp = (function() {
       registerFormatBlots();
       instEditor();
 
-      //init formatting Bar
+      //init formatting bar
       let fmtBar = setUpFormattingBar();
       fmtBar.setUpBtns();
       fmtBar.resetToolbarView();
